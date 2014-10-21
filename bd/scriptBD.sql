82cd54f2235fb5f93a3681be1f65cf3987b1b1f3
Aluno <Aluno@10.1.20.48>
2014-10-21 16:37:36 -0200

    Aula 31 Finalização do modelo ER e Script do Banco #3

---
+++ bd/scriptBD.sql
@@ -0,0 +1,110 @@
+SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
+SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
+SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL';
+
+CREATE SCHEMA IF NOT EXISTS `mydb` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci ;
+USE `mydb` ;
+
+-- -----------------------------------------------------
+-- Table `mydb`.`usuario`
+-- -----------------------------------------------------
+CREATE  TABLE IF NOT EXISTS `mydb`.`usuario` (
+  `idusuario` INT NOT NULL AUTO_INCREMENT ,
+  `nome` VARCHAR(45) NOT NULL ,
+  `email` VARCHAR(45) NOT NULL ,
+  `idade` INT NOT NULL ,
+  `foto` VARCHAR(45) NULL ,
+  PRIMARY KEY (`idusuario`) ,
+  UNIQUE INDEX `email_UNIQUE` (`email` ASC) )
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `mydb`.`artigo`
+-- -----------------------------------------------------
+CREATE  TABLE IF NOT EXISTS `mydb`.`artigo` (
+  `idartigo` INT NOT NULL AUTO_INCREMENT ,
+  `titulo` VARCHAR(45) NOT NULL ,
+  `conteudo` VARCHAR(45) NOT NULL ,
+  `usuario_idusuario` INT NOT NULL ,
+  PRIMARY KEY (`idartigo`) ,
+  UNIQUE INDEX `titulo_UNIQUE` (`titulo` ASC) ,
+  INDEX `fk_artigo_usuario` (`usuario_idusuario` ASC) ,
+  CONSTRAINT `fk_artigo_usuario`
+    FOREIGN KEY (`usuario_idusuario` )
+    REFERENCES `mydb`.`usuario` (`idusuario` )
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `mydb`.`fotos`
+-- -----------------------------------------------------
+CREATE  TABLE IF NOT EXISTS `mydb`.`fotos` (
+  `idfotos` INT NOT NULL AUTO_INCREMENT ,
+  `nome` VARCHAR(45) NOT NULL ,
+  `artigo_idartigo` INT NOT NULL ,
+  PRIMARY KEY (`idfotos`) ,
+  INDEX `fk_fotos_artigo1` (`artigo_idartigo` ASC) ,
+  CONSTRAINT `fk_fotos_artigo1`
+    FOREIGN KEY (`artigo_idartigo` )
+    REFERENCES `mydb`.`artigo` (`idartigo` )
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `mydb`.`comentario`
+-- -----------------------------------------------------
+CREATE  TABLE IF NOT EXISTS `mydb`.`comentario` (
+  `idcomentario` INT NOT NULL ,
+  `data` DATETIME NULL ,
+  `usuario_idusuario` INT NOT NULL ,
+  PRIMARY KEY (`idcomentario`) ,
+  INDEX `fk_comentario_usuario1` (`usuario_idusuario` ASC) ,
+  CONSTRAINT `fk_comentario_usuario1`
+    FOREIGN KEY (`usuario_idusuario` )
+    REFERENCES `mydb`.`usuario` (`idusuario` )
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `mydb`.`categoria`
+-- -----------------------------------------------------
+CREATE  TABLE IF NOT EXISTS `mydb`.`categoria` (
+  `idcategoria` INT NOT NULL AUTO_INCREMENT ,
+  `tema` VARCHAR(45) NOT NULL ,
+  PRIMARY KEY (`idcategoria`) )
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `mydb`.`categoria_has_artigo`
+-- -----------------------------------------------------
+CREATE  TABLE IF NOT EXISTS `mydb`.`categoria_has_artigo` (
+  `categoria_idcategoria` INT NOT NULL ,
+  `artigo_idartigo` INT NOT NULL ,
+  PRIMARY KEY (`categoria_idcategoria`, `artigo_idartigo`) ,
+  INDEX `fk_categoria_has_artigo_artigo1` (`artigo_idartigo` ASC) ,
+  INDEX `fk_categoria_has_artigo_categoria1` (`categoria_idcategoria` ASC) ,
+  CONSTRAINT `fk_categoria_has_artigo_categoria1`
+    FOREIGN KEY (`categoria_idcategoria` )
+    REFERENCES `mydb`.`categoria` (`idcategoria` )
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_categoria_has_artigo_artigo1`
+    FOREIGN KEY (`artigo_idartigo` )
+    REFERENCES `mydb`.`artigo` (`idartigo` )
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+
+SET SQL_MODE=@OLD_SQL_MODE;
+SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
+SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
